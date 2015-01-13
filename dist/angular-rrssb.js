5f4871daa381a92a675a28227aa79b3a2d7f7112
Unai Recio <unai.recio@agcpartners.co.uk>
2015-01-13 11:42:52 +0000

    fix(rrssb directive): Typo 'replcae' is now 'replace'

--- dist/angular-rrssb.js
+++ dist/angular-rrssb.js
@@ -1,7 +1,7 @@
 /*!
  * angular-rrssb
  * https://github.com/mvsouza/angular-rrssb
- * Version: 0.0.8 - 2015-01-12T16:01:30.458Z
+ * Version: 0.0.8 - 2015-01-13T11:41:50.144Z
  * License: MIT
  */
 
@@ -13,7 +13,7 @@ angular.module('mvsouza.angular-rrssb', []).directive('rrssb', function () {
   return {
     restrict: 'AE',
     templateUrl: 'angular-rrssb.html',
-    replcae: true,
+    replace: true,
     link: function ($scope, element, attr) {
       $scope.urlToShare = attr.ngShareLink;
       $scope.shareMidias = attr.ngShareMidias;
